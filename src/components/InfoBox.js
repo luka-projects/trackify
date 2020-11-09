ef0d308753dcd9bc0ba5f5f5f3920bb209871ae9
lukautvic <67318904+lukautvic@users.noreply.github.com>
2020-11-09 19:57:04 +0100

    /

--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -1,17 +1,23 @@
 import { Card, CardContent, Typography } from '@material-ui/core'
 import './InfoBox.css'
 
-function InfoBox({ title, cases, total }) {
+function InfoBox({ title, active, isRed, cases, total, ...props }) {
     return (
-        <Card className='infoBox'>
+        <Card
+            className={`infoBox ${active && 'infoBox--selected'} ${isRed && 'infoBox--red'}`}
+            onClick={props.onClick}>
             <CardContent>
-                <Typography className='infoBox__title' color='textSecondary'>
+                <Typography className='infoBox__title' color='textSecondary' >
                     {title}
                 </Typography>
 
-                <h2 className='infoBox__cases'>{cases}</h2>
+                <h2
+                    className={`infoBox__cases ${!isRed && '.infoBox__cases--green'}`}
+                >
+                    {cases}
+                </h2>
 
-                <Typography className='infoBox__total' color='textSecondary'> 
+                <Typography className='infoBox__total' color='textSecondary'>
                     {total} Total
                 </Typography>
             </CardContent>
--- src/utils.js
+++ src/utils.js
@@ -3,16 +3,16 @@ import numeral from 'numeral'
 
 const casesTypeColors = {
     cases: {
-        hex: "#CC1034",
         size: 400,
+        option: { color: "#ff6c47", fillColor: "#cc1034" },
     },
     recovered: {
-        hex: "#7dd71d",
         size: 800,
+        option: { color: "#7dd71d", fillColor: "#7dd71d" },
     },
     deaths: {
-        hex: "#fb4443",
         size: 1600,
+        option: { color: "#cc1034", fillColor: "#ff6c47" }
     },
 };
 
@@ -21,28 +21,34 @@ export const showData = (data, casesType = 'cases') => (
         <Circle
             center={[country.countryInfo.lat, country.countryInfo.long]}
             fillOpacity={0.4}
-            color={casesTypeColors[casesType].hex}
-            fillColor={casesTypeColors[casesType].hex}
+            pathOptions={casesTypeColors[casesType].option}
             radius={
                 Math.sqrt(country[casesType]) * casesTypeColors[casesType].size
             }
         >
-
             <Popup>
-                <div className='popup__container'>
-                    <div className='popup__flag' style={{ backgroundImage: `url(${country.countryInfo.flag})` }}></div>
-                    <div className='popup__name'>{country.country}</div>
-                    <div className='popup__cases'>
-                        Cases: {numeral(country.cases).format('0,0')}
+                <div className="popup__container">
+                    <div
+                        className="popup__flag"
+                        style={{ backgroundImage: `url(${country.countryInfo.flag})` }}
+                    ></div>
+                    <div className="popup__name">{country.country}</div>
+                    <div className="popup__cases">
+                        Cases: {numeral(country.cases).format("0,0")}
                     </div>
-                    <div className='popup__recoveries'>
-                        Recoveries: {numeral(country.recovered).format('0,0')}
+                    <div className="popup__recoveries">
+                        Recovered: {numeral(country.recovered).format("0,0")}
                     </div>
-                    <div className='popup__deaths'>
-                        Deaths: {numeral(country.deaths).format('0,0')}
+                    <div className="popup__deaths">
+                        Deaths: {numeral(country.deaths).format("0,0")}
                     </div>
                 </div>
             </Popup>
         </Circle>
     ))
 )
+
+export const prettify = (stat) => (
+    stat ? `+${numeral(stat).format('0.0a')}` : '+0'
+)
+
